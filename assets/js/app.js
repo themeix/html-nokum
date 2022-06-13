046716009428dfd08c24c554dfd3dd2f80660d01
Arifulislam03 <75440947+Arifulislam03@users.noreply.github.com>
2022-06-13 17:26:43 +0600

    menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
- (function ($) {
-   "use strict";
+(function ($) {
+  "use strict";
 
 
   /*
@@ -7,58 +7,57 @@
    Body Sticky 
    --------------------------
    */
-   $(window).on('scroll', function () {
+  $(window).on('scroll', function () {
     var scroll = $(window).scrollTop();
     if (scroll >= 150) {
       $(".body-scroll").addClass("body-sticky");
-      if ($(".close-button")){
-        $(".header-area").addClass("header-sticky");
+      if ($('.nokum-menu-icon').hasClass('close-button')) {
+        $('.header-area').addClass('header-sticky');
       }
     } else {
       $(".body-scroll").removeClass("body-sticky");
-      $(".header-area").removeClass("header-sticky");
+      $('.header-area').removeClass('header-sticky');
     }
   });
 
 
 
 
-    /*  Active Class*/
-    $('.nokum-menu-icon').on('click', function () {
-      $(this).toggleClass("close-button")
-      $(".header-area ").toggleClass('header-sticky');
-     
-    });
+  /*  Active Class*/
+  $('.nokum-menu-icon').on('click', function () {
+    $(this).toggleClass("close-button")
+    $(".header-area ").toggleClass('header-sticky');
+  })
 
 
 
-   /*
-   ------------------------  
-   Mobile Menu 
-   --------------------------
-   */
-   $(".mobile-toggle").on("click", function () {
-     $(this).toggleClass("open");
-     $(".toggle-menu-class").slideToggle();
-   });
-   $(".toggle").on("click", function () {
-     if ($(this).text().includes("-")) {
-       $(this).text("+")
-     } else {
-       $(this).text("-")
-     }
-     $(this).parent().siblings(".submenu").slideToggle();
-   });
+  /*
+  ------------------------  
+  Mobile Menu 
+  --------------------------
+  */
+  $(".mobile-toggle").on("click", function () {
+    $(this).toggleClass("open");
+    $(".toggle-menu-class").slideToggle();
+  });
+  $(".toggle").on("click", function () {
+    if ($(this).text().includes("-")) {
+      $(this).text("+")
+    } else {
+      $(this).text("-")
+    }
+    $(this).parent().siblings(".submenu").slideToggle();
+  });
 
 
 
-   /*
-   ------------------------  
-   Current Date
-   --------------------------
-   */
-   $('#spanYear').html(new Date().getFullYear());
+  /*
+  ------------------------  
+  Current Date
+  --------------------------
+  */
+  $('#spanYear').html(new Date().getFullYear());
 
-  
 
- }(jQuery));
+
+}(jQuery));
